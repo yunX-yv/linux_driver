0bb44725dd8d0c77975e7bdcdb3b44ee15759910
xiaodq <2099330740@qq.com>
2025-06-23 17:30:19 +0800

    添加字符设备

--- si5351A/si5351a.c
+++ si5351A/si5351a.c
@@ -1,17 +1,77 @@
+#include <linux/clk.h>
+#include <linux/device.h>
 #include <linux/init.h>
 #include <linux/module.h>
-#include <linux/platform_device.h>
-#include <linux/clk.h>
 #include <linux/of.h>
+#include <linux/platform_device.h>
+
+#define SI5351A_CLASS_NAME "si5351x_Out"
+#define SI5351A_DEV_NAME  "si5351x"
+
+struct output_clock_device {
+  struct device *dev;
+  struct clk **clks;
+  int num_clks;
+};
+static struct output_clock_device *clk_dev;
 
 int si5351a_user_probe(struct platform_device *pdev) {
   struct clk *clk;
-  clk = clk_get(NULL, "my_gateable_clk");
+  const char *const *clk_names = NULL;
+  int i, count;
+
+  clk_dev = devm_kzalloc(&pdev->dev, sizeof(*clk_dev), GFP_KERNEL);
+  if (!clk_dev)
+    return -ENOMEM;
+
+  clk_dev->dev = &pdev->dev;
+
+  /* first, get clocks from device tree */
+  count =
+      of_count_phandle_with_args(pdev->dev.of_node, "clocks", "#clock-cells");
+  if (count <= 0) {
+    dev_err(&pdev->dev, "No clocks defined in device tree\n");
+    goto err_free;
+  }
+  clk_dev->num_clks = count;
+
+  clk_names = of_get_property(pdev->dev.of_node, "clock-names", NULL);
+  if (!clk_names) {
+    pr_err("No clock-names property found\n");
+    goto err_free;
+  }
+
+  for (i = 0; i < count; i++) {
+    clk = devm_clk_get(&pdev->dev, clk_names[i]);
+    if (IS_ERR(clk)) {
+      dev_err(&pdev->dev, "Failed to get clock %d\n", i);
+      return PTR_ERR(clk);
+    }
+    dev_info(&pdev->dev, "Clock %d: %s\n", i, clk_names[i]);
+    clk_dev->clks[i] = clk;
+    clk_unprepare(clk);
+  }
+
   return 0;
+
+err_free:
+  for (i = 0; i < clk_dev->num_clks; i++) {
+    if (clk_dev->clks[i]) {
+      clk_unprepare(clk_dev->clks[i]);
+      devm_clk_put(&pdev->dev, clk_dev->clks[i]);
+    }
+  }
+  devm_kfree(&pdev->dev, clk_dev->clks);
+  return -ENOMEM;
 }
 
 int si5351a_user_remove(struct platform_device *pdev) {
-
+  int i;
+  for (i = 0; i < clk_dev->num_clks; i++) {
+    if (clk_dev->clks[i])
+      devm_clk_put(&pdev->dev, clk_dev->clks[i]);
+  }
+  devm_kfree(&pdev->dev, clk_dev->clks);
   return 0;
 }
 
@@ -23,7 +83,7 @@ static const struct platform_device_id si5351a_id_table[] = {
 static const struct of_device_id si5351a_of_table[] = {
     {.compatible = "Silicon,si5351a-usr"}, {}};
 
-static struct platform_driver si5351a_user_drv = {
+static struct platform_driver user_drv = {
     .probe = si5351a_user_probe,
     .remove = si5351a_user_remove,
     .driver =
@@ -35,14 +95,14 @@ static struct platform_driver si5351a_user_drv = {
     .id_table = si5351a_id_table,
 };
 
-static int __init si5351a_user_output_init(void) {
-  return platform_driver_register(&si5351a_user_drv);
+static int __init user_output_init(void) {
+  return platform_driver_register(&user_drv);
 }
 
-static void __exit si5351a_user_output_exit(void) {
-  platform_driver_unregister(&si5351a_user_drv);
+static void __exit user_output_exit(void) {
+  platform_driver_unregister(&user_drv);
 }
 
-module_init(si5351a_user_output_init);
-module_exit(si5351a_user_output_exit);
+module_init(user_output_init);
+module_exit(user_output_exit);
 MODULE_LICENSE("GPL");
